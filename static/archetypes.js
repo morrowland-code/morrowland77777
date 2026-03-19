8b767d096e9155fac55105aea38f67faf0c729cc
morrowland-code <moorekendall346@gmail.com>
2026-03-19 17:21:13 -0400

    Delete static directory

--- static/archetypes.js
+++
@@ -1,6 +0,0 @@
-const archetypes = {
-  "Low-Low-Low-Low-Low": "Aquashine",
-  "High-High-High-High-High": "Emberheart",
-  "Medium-Medium-Medium-Medium-Medium": "Imagine",
-  // Add more as needed…
-};
